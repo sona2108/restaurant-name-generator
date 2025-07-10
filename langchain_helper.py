99db257367c913cde37d7c592da9af06509e14fd
Sona <sona.sharma21x8@gmail.com>
2025-07-10 12:47:12 +0530

    second

---
+++ langchain_helper.py
@@ -0,0 +1,39 @@
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+os.environ['GOOGLE_API_KEY'] = 'AIzaSyCfyLG3Ru-ECVyI8FispiLxGaVohwCZ3k0'
+
+llm = ChatGoogleGenerativeAI(model="models/gemini-1.5-flash", temperature=1)
+
+def generate_restaurant_name_and_items(cuisine):
+    # Chain 1: Restaurant Name
+    prompt_template_name = PromptTemplate(
+        input_variables=['cuisine'],
+        template="I want to open a restaurant for {cuisine} food. Suggest a fancy name for this."
+    )
+
+    name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="restaurant_name")
+
+    # Chain 2: Menu Items
+    prompt_template_items = PromptTemplate(
+        input_variables=['restaurant_name'],
+        template="""Suggest some menu items for {restaurant_name}. Return it as a comma separated string"""
+    )
+
+    food_items_chain = LLMChain(llm=llm, prompt=prompt_template_items, output_key="menu_items")
+
+    chain = SequentialChain(
+        chains=[name_chain, food_items_chain],
+        input_variables=['cuisine'],
+        output_variables=['restaurant_name', "menu_items"]
+    )
+
+    response = chain({'cuisine': cuisine})
+
+    return response
+
+if __name__ == "__main__":
+    print(generate_restaurant_name_and_items("Italian"))
